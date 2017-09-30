b08853ef611aad81d89838b1d1b540996dad0f63
vt020 <32144433+vt020@users.noreply.github.com>
2017-09-30 16:06:21 -0400

    Cleaning up Sorter.h

--- Sorter.h
+++ Sorter.h
@@ -1,26 +1,14 @@
-/*****
-*
-*	Define structures and function prototypes for your sorter
-*
-*
-*
-******/
-
-//Suggestion: define a struct that mirrors a record (row) of the data set
 #ifndef _SORTER_H_
 #define _SORTER_H_
 
-typedef struct _data { //THIS DATA STRUCT IS CURRENTLY BAD, PUT DATA IN NODE
+typedef struct _data { 
 char** fielddata; //2d array to contain string types
 int* comma; //does the node have a comma in the field n? 0 = no, 1 = yes
-//might want to make the data types a linked list also?
-//or make a LL to keep track when creating, then place into array??
 } data;
 
 typedef struct _Node {
 data ndata; //data in a node
 
-//char** data; use this instead of a data struct
 struct _Node* next;
 }Node;
 
@@ -35,7 +23,6 @@ int* types; //keeps track of types, 0 = string, 1 = long, 2 = float
 char** fields; //keeps track of the name fields
 }LL;
 
-//Suggestion: prototype a mergesort function
 int mergesortBegin(LL* dlist, char* field);
 Node* mergeSort(LL* dlist, Node* head);
 Node* split(Node* head);
